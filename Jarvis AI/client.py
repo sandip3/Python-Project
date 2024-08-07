1b91af379ad2be6bf043fb2a668005703bde98ae
Sandip Mishra <sm7405243902@gmail.com>
2024-08-07 20:28:53 +0530

    Jarvis AI

--- Jarvis AI/client.py
+++ Jarvis AI/client.py
@@ -3,7 +3,7 @@
 # # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
 # # if you saved the key under a different environment variable name, you can do something like:
 # client = OpenAI(
-#     api_key="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",
+#     api_key="",
 # )
 
 # completion = client.chat.completions.create(
